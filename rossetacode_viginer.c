3e956b4b2503f08ed93168620c9e1dafa9655670
electron101 <sabashlive@gmail.com>
2017-12-08 15:20:36 +0300

    Начал переписывать на чистый си.

--- rossetacode_viginer.c
+++ rossetacode_viginer.c
@@ -15,31 +15,6 @@ typedef struct _freq {
 	double _double_freq;
 } FreqArray;
 
-char input[] =
-"MOMUD EKAPV TQEFM OEVHP AJMII CDCTI FGYAG JSPXY ALUYM NSMYH"
-"VUXJE LEPXJ FXGCM JHKDZ RYICU HYPUS PGIGM OIYHF WHTCQ KMLRD"
-"ITLXZ LJFVQ GHOLW CUHLO MDSOE KTALU VYLNZ RFGBX PHVGA LWQIS"
-"FGRPH JOOFW GUBYI LAPLA LCAFA AMKLG CETDW VOELJ IKGJB XPHVG"
-"ALWQC SNWBU BYHCU HKOCE XJEYK BQKVY KIIEH GRLGH XEOLW AWFOJ"
-"ILOVV RHPKD WIHKN ATUHN VRYAQ DIVHX FHRZV QWMWV LGSHN NLVZS"
-"JLAKI FHXUF XJLXM TBLQV RXXHR FZXGV LRAJI EXPRV OSMNP KEPDT"
-"LPRWM JAZPK LQUZA ALGZX GVLKL GJTUI ITDSU REZXJ ERXZS HMPST"
-"MTEOE PAPJH SMFNB YVQUZ AALGA YDNMP AQOWT UHDBV TSMUE UIMVH"
-"QGVRW AEFSP EMPVE PKXZY WLKJA GWALT VYYOB YIXOK IHPDS EVLEV"
-"RVSGB JOGYW FHKBL GLXYA MVKIS KIEHY IMAPX UOISK PVAGN MZHPW"
-"TTZPV XFCCD TUHJH WLAPF YULTB UXJLN SIJVV YOVDJ SOLXG TGRVO"
-"SFRII CTMKO JFCQF KTINQ BWVHG TENLH HOGCS PSFPV GJOKM SIFPR"
-"ZPAAS ATPTZ FTPPD PORRF TAXZP KALQA WMIUD BWNCT LEFKO ZQDLX"
-"BUXJL ASIMR PNMBF ZCYLV WAPVF QRHZV ZGZEF KBYIO OFXYE VOWGB"
-"BXVCB XBAWG LQKCM ICRRX MACUO IKHQU AJEGL OIJHH XPVZW JEWBA"
-"FWAML ZZRXJ EKAHV FASMU LVVUT TGK";
-
-double english[26] = {
-	0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015,
-	0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749,
-	0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758,
-	0.00978, 0.02360, 0.00150, 0.01974, 0.00074
-};
 
 double targets[26];
 double sortedTargets[26];
@@ -50,7 +25,7 @@ typedef struct _output {
 	char *str2;
 } output;
 
-FreqArray frequency(char *input) 
+FreqArray frequency(const char *input) 
 {
 	for (char c = 'A'; c <= 'Z'; ++c) {
 		freq[c - 'A']._char_freq = c;
@@ -63,7 +38,7 @@ FreqArray frequency(char *input)
 	return *freq;
 }
 
-double correlation(char *input) 
+double correlation(const char *input) 
 {
 	double result = 0.0;
 
@@ -71,6 +46,9 @@ double correlation(char *input)
 
 	int i = 0;
 	int j = 0;
+	/* Сортируем по возрастанию частоты символа 
+	 * ( второго поля структуры)
+	 */
 	for(i = 0 ; i < 26 - 1; i++) { 
 		/* сравниваем два соседних элемента. */
 		for(j = 0 ; j < 26 - i - 1 ; j++) {  
@@ -90,8 +68,30 @@ double correlation(char *input)
 	return result;
 }
 
+void VigenereAnalyser(double targetFreqs[26]) 
+{
+	size_t i;
+	size_t j;
+	for (i = 0; i < 26; i++)
+	{
+		targets[i] = targetFreqs[i];
+		sortedTargets[i] = targets[i];
+	}
+	
+	/* Сортируем по возрастанию */
+	for(i = 0 ; i < 26 - 1; i++) { 
+		for(j = 0 ; j < 26 - i - 1 ; j++) {  
+			if(sortedTargets[j] > sortedTargets[j+1]) {
+				/* по возрастанию */
+				double tmp	   = sortedTargets[j];
+				sortedTargets[j]   = sortedTargets[j+1] ;
+				sortedTargets[j+1] = tmp; 
+			}
+		}
+	}
+}
 
-output analyze() 
+output analyze(char *input) 
 {
 	output result_output;
 	char cleaned[strlen(input) + 1];
@@ -125,16 +125,21 @@ output analyze()
 	 */
 	for (i = 2; i < strlen(cleaned) / 20; ++i) 
 	{
-		char *pieces[i];
-		/* vector<string> pieces(i); */
-		for (size_t j = 0; j < strlen(cleaned); ++j)
-		{
-			pieces[j % i] += cleaned[j];
-			/* size_t cur_len = strlen(pieces); */
-			/* if(cur_len < strlen(input)) { */
-			/* 	pieces[cur_len] = cleaned[j]; */
-			/* 	pieces[cur_len + 1] = '\0'; */
-			/* } */
+		char **pieces;
+		pieces = (char **) malloc(i * sizeof(char *));
+		int n;	/* n - колличество строк в массиве */
+		/* m - длина каждой строки + 1 для запаса */
+		int m = (int) strlen(cleaned) / i  + 1;
+		for (n = 0; n < i; n++) {
+			pieces[n] = (char *) malloc(m * sizeof(char));
+		}
+
+		for (size_t j = 0; j < strlen(cleaned); ++j) {
+			size_t cur_len = strlen(pieces[j % i]);
+			if(cur_len < m) {
+				pieces[j % i][cur_len] = cleaned[j];
+				pieces[j % i][cur_len + 1] = '\0';
+			}
 		}
 
 		/* Корреляция искусственно возрастает для меньших
@@ -159,14 +164,18 @@ output analyze()
 	}
 
 	char *pieces[bestLength];
+	/* m - длина строки полученной при разделении 
+	 * всего шифрованного текста на отрезки количеством bestLength.
+	 * + 1 для запаса
+	 */
+	int m = (int) strlen(cleaned) / bestLength  + 1;
 	for (i = 0; i < strlen(cleaned); ++i)
 	{
-		pieces[i % bestLength] += cleaned[i];
-		/* size_t cur_len = strlen(pieces); */
-		/* if(cur_len < strlen(s)) { */
-		/* 	pieces[cur_len] = s[i]; */
-		/* 	pieces[cur_len + 1] = '\0'; */
-		/* } */
+		size_t cur_len = strlen(pieces[i % bestLength]);
+		if(cur_len < m) {
+			pieces[i % bestLength][cur_len] = cleaned[i];
+			pieces[i % bestLength][cur_len + 1] = '\0';
+		}
 		
 	}
 
@@ -224,107 +233,43 @@ output analyze()
 	return make_pair(result, key);
 }
 
-/* void VigenereAnalyser(double targetFreqs[26]) */
-/* { */
-/* 	targets = targetFreqs; */
-/*  */
-/*  */
-/* } */
-/*  */
-/* int best_match(const double *a, const double *b) { */
-/* 	double sum = 0, fit, d, best_fit = 1e100; */
-/* 	int i, rotate, best_rotate = 0; */
-/* 	for (i = 0; i < 26; i++) */
-/* 		sum += a[i]; */
-/* 	for (rotate = 0; rotate < 26; rotate++) { */
-/* 		fit = 0; */
-/* 		for (i = 0; i < 26; i++) { */
-/* 			d = a[(i + rotate) % 26] / sum - b[i]; */
-/* 			fit += d * d / b[i]; */
-/* 		} */
-/*  */
-/* 		if (fit < best_fit) { */
-/* 			best_fit = fit; */
-/* 			best_rotate = rotate; */
-/* 		} */
-/* 	} */
-/*  */
-/* 	return best_rotate; */
-/* } */
-/*  */
-/* double freq_every_nth(const int *msg, int len, int interval, char *key) { */
-/* 	double sum, d, ret; */
-/* 	double out[26], accu[26] = {0}; */
-/* 	int i, j, rot; */
-/*  */
-/* 	for (j = 0; j < interval; j++) { */
-/* 		for (i = 0; i < 26; i++) */
-/* 			out[i] = 0; */
-/* 		for (i = j; i < len; i += interval) */
-/* 			out[msg[i]]++; */
-/* 		key[j] = rot = best_match(out, freq); */
-/* 		key[j] += 'A'; */
-/* 		for (i = 0; i < 26; i++) */
-/* 			accu[i] += out[(i + rot) % 26]; */
-/* 	} */
-/*  */
-/* 	for (i = 0, sum = 0; i < 26; i++) */
-/* 		sum += accu[i]; */
-/*  */
-/* 	for (i = 0, ret = 0; i < 26; i++) { */
-/* 		d = accu[i] / sum - freq[i]; */
-/* 		ret += d * d / freq[i]; */
-/* 	} */
-/*  */
-/* 	key[interval] = '\0'; */
-/* 	return ret; */
-/* } */
-  
-// Update the freqs array
-
-
-
 
 
 int main() 
 {
+	char input[] =
+		"MOMUD EKAPV TQEFM OEVHP AJMII CDCTI FGYAG JSPXY ALUYM NSMYH"
+		"VUXJE LEPXJ FXGCM JHKDZ RYICU HYPUS PGIGM OIYHF WHTCQ KMLRD"
+		"ITLXZ LJFVQ GHOLW CUHLO MDSOE KTALU VYLNZ RFGBX PHVGA LWQIS"
+		"FGRPH JOOFW GUBYI LAPLA LCAFA AMKLG CETDW VOELJ IKGJB XPHVG"
+		"ALWQC SNWBU BYHCU HKOCE XJEYK BQKVY KIIEH GRLGH XEOLW AWFOJ"
+		"ILOVV RHPKD WIHKN ATUHN VRYAQ DIVHX FHRZV QWMWV LGSHN NLVZS"
+		"JLAKI FHXUF XJLXM TBLQV RXXHR FZXGV LRAJI EXPRV OSMNP KEPDT"
+		"LPRWM JAZPK LQUZA ALGZX GVLKL GJTUI ITDSU REZXJ ERXZS HMPST"
+		"MTEOE PAPJH SMFNB YVQUZ AALGA YDNMP AQOWT UHDBV TSMUE UIMVH"
+		"QGVRW AEFSP EMPVE PKXZY WLKJA GWALT VYYOB YIXOK IHPDS EVLEV"
+		"RVSGB JOGYW FHKBL GLXYA MVKIS KIEHY IMAPX UOISK PVAGN MZHPW"
+		"TTZPV XFCCD TUHJH WLAPF YULTB UXJLN SIJVV YOVDJ SOLXG TGRVO"
+		"SFRII CTMKO JFCQF KTINQ BWVHG TENLH HOGCS PSFPV GJOKM SIFPR"
+		"ZPAAS ATPTZ FTPPD PORRF TAXZP KALQA WMIUD BWNCT LEFKO ZQDLX"
+		"BUXJL ASIMR PNMBF ZCYLV WAPVF QRHZV ZGZEF KBYIO OFXYE VOWGB"
+		"BXVCB XBAWG LQKCM ICRRX MACUO IKHQU AJEGL OIJHH XPVZW JEWBA"
+		"FWAML ZZRXJ EKAHV FASMU LVVUT TGK";
+
+	double english[26] = {
+		0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015,
+		0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749,
+		0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758,
+		0.00978, 0.02360, 0.00150, 0.01974, 0.00074
+	};
+
+	VigenereAnalyser(english);
+
 	int j = 0;
 
-	// сделать сортировку структуры
-	//
-	
-	for (j = 0; j < 26; j++)
-	{
-		targets[j] = english[j];
-		printf ("%f", targets[j]);
-	}
+	struct _output output; 
+	output = analyze(input);
 
-	/* struct _output output;  */
-	/* output = analyze(input); */
-
-
-
-
-
-
-	/* int txt[strlen(encoded)]; */
-	/* int len = 0, j; */
-	/* char key[100]; */
-	/* double fit, best_fit = 1e100; */
-        /*  */
-	/* for (j = 0; encoded[j] != '\0'; j++) */
-	/* 	if (isupper(encoded[j])) */
-	/* 		txt[len++] = encoded[j] - 'A'; */
-        /*  */
-	/* for (j = 1; j < 30; j++) { */
-	/* 	fit = freq_every_nth(txt, len, j, key); */
-	/* 	printf("%f, key length: %2d, %s", fit, j, key); */
-	/* 	if (fit < best_fit) { */
-	/* 		best_fit = fit; */
-	/* 		printf(" <--- best so far"); */
-	/* 	} */
-	/* 	printf("\n"); */
-	/* } */
 
 	return 0;
 }
